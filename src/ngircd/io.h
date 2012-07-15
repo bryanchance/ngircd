239dcbb0337c37dbc4d771de9319afb1b275e4ff
Nahum Shalman <nahamu@gmail.com>
2012-07-15 10:37:26 -0400

    initial port of ngircd over to libevent
    This patch replaces all of the custom ifdefs in io.c with
    a single implementation based on libevent which supports
    all of the previously existing backends, plus aditional ones.

--- src/ngircd/io.h
+++ src/ngircd/io.h
@@ -16,9 +16,10 @@
 
 #include "portab.h"
 #include <sys/time.h>
+#include <event2/event.h>
 
-#define IO_WANTREAD	1
-#define IO_WANTWRITE	2
+#define IO_WANTREAD	EV_READ
+#define IO_WANTWRITE	EV_WRITE
 
 /* init library.
    sets up epoll/kqueue descriptors and tries to allocate space for ioevlen
@@ -29,10 +30,10 @@ bool io_library_init PARAMS((unsigned int ioevlen));
 void io_library_shutdown PARAMS((void));
 
 /* add fd to internal set, enable readability check, set callback */
-bool io_event_create PARAMS((int fd, short what, void (*cbfunc)(int, short)));
+bool io_event_create PARAMS((int fd, short what, void (*cbfunc)(int, short, void*)));
 
 /* change callback function associated with fd */
-bool io_event_setcb PARAMS((int fd, void (*cbfunc)(int, short)));
+bool io_event_setcb PARAMS((int fd, void (*cbfunc)(int, short, void*)));
 
 /* watch fd for event of type what */
 bool io_event_add PARAMS((int fd, short what));
